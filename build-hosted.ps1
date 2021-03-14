a991ef8bf07c6f3584fa8ce5ee22c2ffb370ba60
Rick Strahl <rstrahl@west-wind.com>
2021-03-13 15:55:11 -1000

    1

--- build-hosted.ps1
+++ build-hosted.ps1
@@ -1,13 +1,7 @@
 
 # Make sure you have this in your project:
 #
-# <PublishSingleFile>true</PublishSingleFile>
-# <PublishTrimmed>true</PublishTrimmed>
-# <RuntimeIdentifier>win-x64</RuntimeIdentifier>
-#
-# and you disable the Package Build
-#
-# <PackAsTool>false</PackAsTool>
+# <DefineConstants>USE_RAZORPAGES;BUILD_PACKAGE</DefineConstants>
 #
 # Note:
 # For Razor Compilation `PublishTrimmed` does not work because
@@ -16,18 +10,10 @@
 # with this LiveReloadServer, you can set /p:PublishTrimmed=true
 # to cut the size of the exe in half.
 
-
+if (test-path './build/Hosted' -PathType Container) { remove-item .\build\Hosted -Recurse -Force }
 
 # Single File Exe output
 dotnet publish -c Release -o ./build/Hosted
-copy ./LiveReloadServer/LiveReloadWebServer.json ./LiveReloadWebServer.json
-
-# Make sure hosted project gets built (in default project output folder)
-dotnet publish -c Release /p:PublishSingleFile=false /p:PublishTrimmed=false 
-
-#Move-Item ./SingleFileExe/LiveReloadServer.exe ./LiveReloadWebServer.exe -force
-#remove-item ./SingleFileExe -Recurse -Force
-
 
 # Sign exe
 .\signtool.exe sign /v /n "West Wind Technologies"   /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 ".\build\Hosted\LiveReloadServer.exe"
